95abe7a8b3cbe67804329a476969981932c8bf9c
thoughtcrime <shane.the.timewaster@gmail.com>
2015-11-10 17:01:40 -0600

    populate data set for archetype testing

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,34 +1,22 @@
 if (Archetypes.find().count() === 0) {
   Archetypes.insert({
     className: 'Knight',
-    description: 'The Knight is a bold leader, devoted to his or her craft.' +
-      '\n\n' +
-      'A leader strives to be an example for others, ever striving to be a beacon of hope for the lost.' +
-      'The Knight is bound to a sacred Oath of Honor to follow in the footsteps of the those who have come' +
-      'before him or her.\n\n' +
-      'The Knight will not only follow best practices, but will teach them to apprentices under their watch.',
-    standards: 'The Knight requires rank 5 in at least 3 disciplines in order to before they may become advisors to the High Council.',
+    description: cat('..\archetype_descriptions\knight'),
+    requirements: 'A Knight requires rank 5 in at least 3 disciplines to be eligible to become an advisor to the High Council.',
     minLevel: 15,
     focus: ['Systems Engineering', 'Multi-disciplinary Studies'],
     canAssist: true,
-    difficultyLevel: 5,
-    backgroundImages: {
-      href: '..\\img\\kinghtDemo.png'
-    }
+    difficulty: 5,
+    firstLevelXP: 1500,
+    backgroundImages: [
+      '..\img\kinghtDemo.png'
+    ]
   });
 
   Archetypes.insert({
     className: 'The Construct',
-    description: 'The Construct was once the obsession of a mortal - The Creator.\\n\\n' +
-      'However, The Construct was left behind after The Creator was injured in a training ' +
-      'exercise with The Construct.After the Creator was gone, The Construct began to ' +
-      'perform its own maintenance, and eventually began to make upgrades and improvements ' +
-      'to itself.\\n\\n' +
-      'After many, many years, The Construct could barely remember those times with its ' +
-      'Creator. There were more important things to do than to search old memory banks. The ' +
-      'Construct had places it wanted to go, things it wanted to see, improvements it wanted to make.',
-    standards: 'The Construct is not a terribly complicated machine, but an interest in robotics, AI, ' +
-      'and Mechanical Engineering is strongly recommended.',
+    description: cat('..\archetype_descriptors\construct'),
+    requirements: 'The Construct is not a terribly complicated machine, but an interest in robotics, AI, and Mechanical Engineering is strongly recommended.',
     minLevel: 1,
     focus: [
       'Mechanical Engineering', 'Robotics', 'Neural Networks',
@@ -37,29 +25,16 @@ if (Archetypes.find().count() === 0) {
     ],
     canAssist: true,
     difficulty: 4,
-    backgroundImages: {
-      href: "..\\img\\roboDemo.png"
-    }
+    firstLevelXP: 100,
+    backgroundImages: [
+      fileLoc: "..\img\roboDemo.png"
+    ]
   });
 
   Archetypes.insert({
     className: 'Summoner',
-    description: "Summoners create existences from within his or her imagination. \
-    n\ n
-    Using arcane knowledge gained from a vast array of languages and scholars,
-    Summoners can create nearly anything they choose,
-    provided,
-    of course,
-    that they are able to decipher the encantations without error.When Summoners mis -
-    translate,
-    or
-    stray from the instructions written
-    for the specific manifestation they are seeking,
-    disatrous(and sometimes hilarious) results
-    are sure to occur.\n\ n
-    But,
-    it is an awesome sight to behold in that moment when the Summoner first lays eyes upon their newly hatched creations ",
-    standards: 'Summoners require significant patients and preparation, often months or years of work go into a project with little certainty of the outcome.',
+    description: cat('..\archtype_descriptions\summoner'),
+    requirements: 'Summoners require significant patients and preparation, often months or years of work go into a project with little certainty of the outcome.',
     minLevel: 1,
     focus: {
       'Computer Science', 'Computer Programming', 'Software Engineering',
@@ -68,35 +43,15 @@ if (Archetypes.find().count() === 0) {
     },
     canAssist: true,
     difficulty: 3,
-    backgroundImages: {
-      href: "..\\img\\sorcererDemo.png"
-    }
+    backgroundImages: [
+      "..\img\sorcererDemo.png"
+    ]
   });
 
   Archetypes.insert({
     className: 'Tinkerer',
-    description: "Tinkerers are the true embodiment of makers. \
-    n\ n
-    Tinkerers are the type of people who can put 3 paper clips,
-    a piece of gum,
-    and a small servo engine together to change the channels on the TV without
-    getting up.Tinkerers are always taking things apart,
-    putting them back together again,
-    taking other things apart,
-    and mixing them with something
-    else
-      that was found last week at the scrap heap.Tinkerers are as known
-    for their subtle madness as they are
-    for their great potential
-    for desctruction.\n\ n
-    When all of life is an experiment,
-    Tinkerers have a tendency to seek answers first and ask forgiveness later
-    .
-    ",
-    standards: 'Tinkerers should be curious, willing to sacrifice something small to make something great, creative and sometimes eccentric. They should try to be safe,
-    but be as inquisitive as possible
-    while doing so.
-    ',
+    description: cat('..\archetype_descriptions\tinkerer'),
+    requirements: 'Tinkerers should be curious, willing to sacrifice something small to make something great, creative and sometimes eccentric. They should try to be safe, but be as inquisitive as possible while doing so.',
     minLevel: 1,
     focus: {
       'Making', '3D Printing', 'Repurposing & Recycling',
@@ -105,9 +60,9 @@ if (Archetypes.find().count() === 0) {
     },
     canAssist: false,
     difficulty: 3,
-    backgroundImages: {
-      href: "..\\img\\tinkerDemo.png"
-    }
+    backgroundImages: [
+      "..\img\tinkerDemo.png"
+    ]
   });
 
 
