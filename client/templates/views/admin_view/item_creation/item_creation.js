761f020de691b6accd1f7b815a8be99abc5cad5a
thoughtcrime <shane.the.timewaster@gmail.com>
2015-12-08 08:44:14 -0600

    added basic item creation tools

--- client/templates/views/admin_view/item_creation/item_creation.js
+++ client/templates/views/admin_view/item_creation/item_creation.js
@@ -11,6 +11,15 @@ Template.newItemView.helpers({
 });
 
 Template.newItemView.events({
-
-
+  'submit': function(event, template) {
+    event.preventDefault();
+  },
+  '.random-ui-button': function(event, template) {
+    event.preventDefault();
+    console.log(event);
+  },
+  '.custom-ui-button': function(event, template) {
+    event.preventDefault();
+    console.log(event);
+  },
 });
